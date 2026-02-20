85f0feef53e6654f02920d47ff3d250f805ddcb1
ancedric <101697896+ancedric@users.noreply.github.com>
2026-02-20 16:56:35 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,26 @@ export default defineConfig({
   server: {
     port: 5174,
     strictPort: true,
-    cors: true,
+    cors: true, 
     hmr: {             // <--- Doit être ICI
       protocol: 'ws',
       host: 'localhost',
       port: 5174,
     },
     headers: {
+      "Access-Control-Allow-Origin": "*", // Autorise tout le monde (pour le dev/test)
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  },
+  preview: { // C'est souvent ce que Render utilise pour servir le build
+    port: 5174,
+    strictPort: true,
+    cors: true,
+    headers: {
       "Access-Control-Allow-Origin": "*",
     }
+  },
   },
   plugins: [
     react(),
